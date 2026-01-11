e3458eb9893b781bda3b4e30f3e2967ac9da3bd7
Eric Lewis <ericl16384@gmail.com>
2026-01-11 15:39:59 -0700

    created package smart_lab_phase3 and copied core python files from smart_lab_phase2

---
+++ ros2_ws/src/smart_lab_phase3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'smart_lab_phase3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ericl',
+    maintainer_email='ericl16384@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
