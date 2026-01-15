e0dac68c249747da74bd69717056a3c436d7df13
Eric Lewis <ericl16384@gmail.com>
2026-01-15 14:14:15 -0700

    renamed to basic_robot_control

--- ros2_ws/src/basic_robot_control/setup.py
+++ ros2_ws/src/basic_robot_control/setup.py
@@ -1,8 +1,6 @@
-import os
-from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'my_robot_sim'
+package_name = 'basic_robot_control'
 
 setup(
     name=package_name,
@@ -12,15 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        
-        # Include all launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        
-        # Include all URDF files
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        
-        # Include all Config/YAML files
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -35,8 +24,6 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            # This is where we register your control scripts later
-            'formation_control = my_robot_sim.formation_control:main',
         ],
     },
 )
--- ros2_ws/src/my_robot_sim/next_steps.md
+++
@@ -1,3 +0,0 @@
-I used Gemini extensively to get to my current (limited) understanding,
-but now I think it is time to fully transition to full tutorials, as I have a
-clear goal for the learning, rather than just to impress.
